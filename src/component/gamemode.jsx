cb6e0f1f7d251c4bdf50a7512efb849c5ed2f02c
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-05-14 20:38:24 +0400

    first

--- src/component/gamemode.jsx
+++ src/component/gamemode.jsx
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 function gamemode() {
   return (
     <div>
+      <Typography variant="h4" gutterBottom color={"#fff"} fontWeight={"bold"}>{"> "}
+         GAME MODE
+      </Typography>
       <NavLink className="link0" to="/streak"> WIN STEAK </NavLink><br />
       <NavLink className="link0" to="/play"> 60 SEC </NavLink>
     </div>
